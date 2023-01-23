d7c1c3fcdfcd2815945947ac1194e09c52469165
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2023-01-23 18:13:06 +0100

    added pokemon types tuple

---
+++ main.py
@@ -0,0 +1,2 @@
+TYPES = ("normal", "fire", "water", "electric", "grass", "ice", "fighting", "poison",
+         "ground", "flying", "psychic", "bug", "rock", "ghost", "dragon", "dark", "steel", "fairy")
