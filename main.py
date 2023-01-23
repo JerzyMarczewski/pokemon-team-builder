c4bc8263522543f1ff74cf1268af9e1fd3e37a3f
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2023-01-23 19:18:31 +0100

    added printEffectivenessFromConsoleInput function

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import numpy
+from sys import argv
 
 TYPES = ("normal", "fire", "water", "electric", "grass", "ice", "fighting", "poison",
          "ground", "flying", "psychic", "bug", "rock", "ghost", "dragon", "dark", "steel", "fairy")
+
+N = len(TYPES)
 
 TABLE = ((1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.5, 0, 1, 1, 0.5, 1),
          (1, 0.5, 0.5, 1, 2, 2, 1, 1, 1, 1, 1, 2, 0.5, 1, 0.5, 1, 2, 1),
@@ -24,11 +27,35 @@ TABLE = ((1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.5, 0, 1, 1, 0.5, 1),
          (1, 0.5, 1, 1, 1, 1, 2, 0.5, 1, 1, 1, 1, 1, 1, 2, 2, 0.5, 1)
          )
 
-N = len(TABLE[0])
 
-print(N)
+def printTable():
+    for i in range(N):
+        for j in range(N):
+            print(f"{TABLE[i][j]:<4}", end=" ")
+        print()
 
-for i in range(N):
-    for j in range(N):
-        print(f"{TABLE[i][j]:<4}", end=" ")
-    print()
+
+def printEffectivenessFromConsoleInput():
+    if len(argv) <= 1:
+        return
+
+    superEffective = set()
+    notSuperEffective = set(TYPES)
+
+    for i in range(1, len(argv)):
+        if argv[i] not in TYPES:
+            return print(f"There is no pokemon with type of {argv[i]}")
+
+        index = TYPES.index(argv[i])
+        for j in range(N):
+            if TABLE[index][j] == 2:
+                superEffective.add(TYPES[j])
+                if TYPES[j] in notSuperEffective:
+                    notSuperEffective.remove(TYPES[j])
+
+    print("Your team is super effective against:\n", ", ".join(superEffective))
+    print("\nYour team is NOT super effective against:\n",
+          ", ".join(notSuperEffective))
+
+
+printEffectivenessFromConsoleInput()
